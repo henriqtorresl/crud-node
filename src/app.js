4f0ad19f0588f61afa72dd4cb862b3e7dd7694ab
henriqtorresl <henriquetlandin@gmail.com>
2023-09-26 19:46:38 -0300

    adicionando método post

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());        // configuração para que o app reconheça JSON nas requisições..
+
 app.get('/', (req, res) => {
-    return res.json({msg: 'olá'})
+    return res.json({'message': 'Olá mundo!'})
 })
+
+app.post('/pessoa', (req, res) => {
+    console.log(req.body);
+    
+    return res.json({
+        'statusCode': 200
+    });
+});
 
 app.listen(3000, () => {
     console.log('API rodando!');
