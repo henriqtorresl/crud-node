d1af84526427772979e75b5395110b9e5989004e
henriqtorresl <henriquetlandin@gmail.com>
2023-09-27 12:43:24 -0300

    configurações de segurança

--- src/app.js
+++ src/app.js
@@ -20,3 +20,10 @@ https.createServer({
     cert: fs.readFileSync('src/SSL/code.crt'),
     key:  fs.readFileSync('src/SSL/code.key')
 }, app).listen(3001, () => console.log('API rodando em https'));
+
+// Agora a aplicação roda tanto em http quanto em https, 
+// como o certificado ssl não é oficial e foi gerado através 
+// de um site gratuito, vai dizer que é não seguro, 
+// mas a configuração para rodar em https é essa...
+// porém se fosse um projeto de uma empresa com
+// um certificado ssl oficial, funcionaria perfeitamente!
