3aa0530e6017b82bbfd2c501b8bf206fc08fc2ae
henriqtorresl <henriquetlandin@gmail.com>
2023-09-27 12:20:43 -0300

    comentário

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import router from './routes.js';
 
+// arquivo de configuração do meu app...
+
 const app = express();
 app.use(express.json());        // configuração para que o app reconheça JSON nas requisições..
 app.use(router);                // configuração para o app reconhecer o router que eu instanciei
