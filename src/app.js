3480dee2fe89c8acd593e8560888c0587f98821e
henriqtorresl <henriquetlandin@gmail.com>
2023-09-27 10:37:30 -0300

    arrumando problema do swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTable, insertPessoa, updatePessoa } from './controller/pessoa.js'
+import { createTable, insertPessoa, updatePessoa, selectPessoa, deletePessoa } from './controller/pessoa.js'
 
 const app = express();
 app.use(express.json());        // configuração para que o app reconheça JSON nas requisições..
@@ -8,6 +8,12 @@ createTable();      // criando a tabela de pessoas
 
 app.get('/', (req, res) => {
     return res.json({'message': 'Olá mundo!'})
+})
+
+app.get('/pessoa', async (req, res) => {
+    const pessoas = await selectPessoa();
+
+    return res.json(pessoas);
 })
 
 app.post('/pessoa', (req, res) => {
@@ -23,6 +29,16 @@ app.put('/pessoa/:id', (req, res) => {
     console.log(req.body);      // pessoa
     console.log(req.params);    // id
     updatePessoa(req.body, req.params.id);
+
+    return res.json({
+        'statusCode': 200
+    });
+});
+
+app.delete('/pessoa/:id', (req, res) => {
+    console.log(req.params);    // id
+
+    deletePessoa(req.params.id);
 
     return res.json({
         'statusCode': 200
--- src/controller/pessoa.js
+++ src/controller/pessoa.js
@@ -6,6 +6,16 @@ export async function createTable() {
     });
 }
 
+export async function selectPessoa() {
+    return openDb().then((db) => {
+        db.all('SELECT * FROM Pessoa')
+        .then((pessoas) => {
+            console.log(pessoas)
+            return pessoas;
+        });
+    });
+}
+
 export async function insertPessoa(pessoa) {
     openDb().then((db) => {
         db.run('INSERT INTO Pessoa (nome, idade) VALUES (?,?)', [pessoa.nome, pessoa.idade]);
@@ -15,5 +25,11 @@ export async function insertPessoa(pessoa) {
 export async function updatePessoa(pessoa, id) {
     openDb().then((db) => {
         db.run('UPDATE Pessoa SET nome=?, idade=? WHERE id=?', [pessoa.nome, pessoa.idade, id]);
+    });
+}
+
+export async function deletePessoa(id) {
+    openDb().then((db) => {
+        db.run('DELETE FROM Pessoa WHERE id=?', [id]);
     });
 }
